6e220afbb7c879152d014fb3484e6d1c42d7c031
Jeremy Wildfire <jwildfire@gmail.com>
2025-10-20 23:00:40 -0400

    Update owner names to use first names only

--- src/owners.py
+++ src/owners.py
@@ -6,34 +6,34 @@ Add owner column to fantasy basketball CSV files
 import csv
 
 owner_mapping = {
-    'Utah Bootleggers': 'Trafton Drew',
-    'The Penthouse Panda Bear': 'Jeremy Wildfire',
-    'Austin Football Team': 'Ryan Mindell',
-    'Toso Viti Toso': 'Ben Wildfire',
-    "Im Trying Jennifer": 'Susheel Reddy',
-    'Fly Nye Guy': 'Samuel Nye',
-    'Miami Mambas': 'John Mapp',
-    "Nowitzkis Fadeaway": 'Alex Tsung',
-    'Bull City Bums': 'Nick Martin',
-    'Baton Rouge Beasts': 'Tony Chen',
-    'Austin CurryBrons': 'Ryan Mindell',  # Austin teams likely same owner
-    'Mapp Stepback': 'John Mapp',        # Mapp in name
-    "JMapps Stepover": 'John Mapp',      # Cleaned name
-    'Team Carter': 'Matt Carter',
-    'Team Davidai': 'Nadav Davidai',
-    'Team Reddy': 'Susheel Reddy',       # Reddy in name
-    'Team Nye': 'Samuel Nye',            # Nye in name
-    'Ari 471': 'Ben Wildfire',           # Cleaned version of Ari 47/1
-    'Ari 47/1': 'Ben Wildfire',
-    'Lilongwe 327': 'Ben Wildfire',
-    'UTEP 2 Steps': 'Jon Scheib',
-    'Joe Biden Would Cross You Over': 'John Mapp',
-    'Beto Would Cross You Over': 'John Mapp',
-    'Mwambo Rd TIBA': 'Davide Ruscelli',
-    'Bull City Bangers': 'Nick Martin',  # Bull City teams likely same owner
-    'Uncanny Logo': 'Alex Tsung',
-    'Teh Mehs': 'Jonathan Crisp',
-    "Kawhis Laugh": 'Chris Carannante'
+    'Utah Bootleggers': 'Trafton',
+    'The Penthouse Panda Bear': 'Jeremy',
+    'Austin Football Team': 'Ryan',
+    'Toso Viti Toso': 'Ben',
+    "Im Trying Jennifer": 'Susheel',
+    'Fly Nye Guy': 'Samuel',
+    'Miami Mambas': 'John',
+    "Nowitzkis Fadeaway": 'Alex',
+    'Bull City Bums': 'Nick',
+    'Baton Rouge Beasts': 'Tony',
+    'Austin CurryBrons': 'Ryan',  # Austin teams likely same owner
+    'Mapp Stepback': 'John',        # Mapp in name
+    "JMapps Stepover": 'John',      # Cleaned name
+    'Team Carter': 'Matt',
+    'Team Davidai': 'Nadav',
+    'Team Reddy': 'Susheel',       # Reddy in name
+    'Team Nye': 'Samuel',            # Nye in name
+    'Ari 471': 'Ben',           # Cleaned version of Ari 47/1
+    'Ari 47/1': 'Ben',
+    'Lilongwe 327': 'Ben',
+    'UTEP 2 Steps': 'Jon',
+    'Joe Biden Would Cross You Over': 'John',
+    'Beto Would Cross You Over': 'John',
+    'Mwambo Rd TIBA': 'Davide',
+    'Bull City Bangers': 'Nick',  # Bull City teams likely same owner
+    'Uncanny Logo': 'Alex',
+    'Teh Mehs': 'Jonathan',
+    "Kawhis Laugh": 'Chris'
 }
 
 # Create simple owners mapping CSV
